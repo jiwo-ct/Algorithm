55250053250a008e961dd4f69f27f489f593af58
Jiwon Kim <53257882+jiwo-ct@users.noreply.github.com>
2024-03-23 17:41:48 +0900

    Add files via upload

---
+++ 백준/<13305>/13305.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    long long result = 0; 
+    int distance[100000]; //다시 생각 
+    int n, min, cur; 
+    
+    scanf("%d",&n); 
+    
+    for (int i=0; i<n-1; i++){
+        scanf("%d",&distance[i]); 
+    }
+    for(int j=0; j<n-1; j++){
+        scanf("%d",&cur);  // 리터당 가격 읽기 
+        if (j==0 || min > cur) min = cur; // min에 첫번째 가격 
+        result += (long long)min * distance[j]; 
+    }
+    
+    printf("%lld",result); 
+}
